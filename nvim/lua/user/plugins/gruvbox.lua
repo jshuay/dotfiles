8d5bd0d9d1848386ced06f3506f3021f4ed5a814
jshuay <jshuay@gmail.com>
2022-12-02 00:55:02 +0000

    Add color to IncSearch

--- nvim/lua/user/plugins/gruvbox.lua
+++ nvim/lua/user/plugins/gruvbox.lua
@@ -21,6 +21,8 @@ vim.api.nvim_create_autocmd('ColorScheme', {
         vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#fe8019' })
 
         vim.api.nvim_set_hl(0, 'Search', {})
-        vim.api.nvim_set_hl(0, 'IncSearch', {})
+        vim.api.nvim_set_hl(0, 'IncSearch', { fg = "#fabd2f", ctermfg = 214, reverse = true })
+
+        vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#928374', ctermfg = 245 })
     end
 })
